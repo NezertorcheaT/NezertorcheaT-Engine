21ad4f46d298ceb4699ee26b9259ee252cf6d738
NezertorcheaT <tigrisha06ps@gmail.com>
2023-09-06 21:56:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console_Engine
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp Console-Engine
+ 
