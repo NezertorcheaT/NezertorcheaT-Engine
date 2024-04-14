7b452f55e4d7701a1363137c7131b53a5fd70d88
NezertorcheaT <tigrisha06ps@gmail.com>
2024-04-14 23:46:48 +0300

    aaaa
    widgets

--- Editor/Program.cs
+++ Editor/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using Engine.Core;
+using Engine.Scene;
 
 namespace Editor
 {
@@ -9,7 +10,17 @@ namespace Editor
         public static void Main(string[] args)
         {
             GameConfig.GetData();
-            
+
+            try
+            {
+                GameConfig.SetupHierarchy(() => HierarchyFactory.CreateHierarchy(GameConfig.Data.MAP, true));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Logger.Log(e, "scene build error");
+            }
+
             Process.Start(new ProcessStartInfo("EditorPreview.exe")
                 {
                     Arguments = GameConfig.Data.MAP,
@@ -19,5 +30,52 @@ namespace Editor
 
             Console.ReadKey();
         }
+    }
+    
+    public abstract class Widget
+    {
+        public uint Wight { get; set; }
+        public uint Height { get; set; }
+        public Widget[] Siblings;
+
+        public abstract string Draw();
+    }
+
+    public class InspectorView : Widget
+    {
+        public GameObject GameObject;
+
+        public InspectorView(GameObject gameObject)
+        {
+            GameObject = gameObject;
+        }
+
+        public override string Draw()
+        {
+            return "";
+        }
+    }
+
+    public class HierarchyView : Widget
+    {
+        private Hierarchy Hierarchy => GameConfig.GameHierarchy!;
+        
+        public override string Draw()
+        {
+            return "";
+        }
+    }
+
+    public abstract class Layout : Widget
+    {
+    }
+
+    public abstract class HorizontalLayout : Widget
+    {
+    }
+
+    public interface IScrollable
+    {
+        
     }
 }
