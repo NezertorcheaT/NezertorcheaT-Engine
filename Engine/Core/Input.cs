384add162c496eb49495a450e1133c25e7511d6c
NezertorcheaT <tigrisha06ps@gmail.com>
2024-04-21 19:00:44 +0300

    GetCursorPosition added

--- Engine/Core/Input.cs
+++ Engine/Core/Input.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 namespace Engine.Core
@@ -989,5 +991,40 @@ namespace Engine.Core
         private static extern short GetAsyncKeyState(int vKey);
 
         public static bool GetKey(Keys key) => (GetAsyncKeyState((int) key) & 0x8000) != 0;
+
+
+        /// <summary>
+        /// Struct representing a point.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Vector2(POINT point)
+            {
+                return new Vector2(point.X, point.Y);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the cursor's position, in screen coordinates.
+        /// </summary>
+        /// <see>See MSDN documentation for further information.</see>
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        /// <summary>
+        /// Retrieves the cursor's position, in screen coordinates.
+        /// </summary>
+        /// <returns>Cursor's position</returns>
+        public static Vector2 GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+
+            return lpPoint;
+        }
     }
 }
