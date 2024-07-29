074e14976e943730a23fce3c40aaf93716b8bf08
NezertorcheaT <tigrisha06ps@gmail.com>
2024-07-29 23:14:42 +0300

    trying vim

--- Engine/Core/Startup.cs
+++ Engine/Core/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -47,7 +47,7 @@ namespace Engine.Core
                     yield return her;
                 }
             }
-            
+
             Logger.Initialise();
 
             Input.ConsoleFontUpdate();
--- Engine/Scene/Time.cs
+++ Engine/Scene/Time.cs
@@ -1,4 +1,4 @@
-﻿using Engine.Core;
+using Engine.Core;
 
 namespace Engine.Scene
 {
